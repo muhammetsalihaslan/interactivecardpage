34158b53266f77b6f85a18aa533f755bdd957a72
Your Name <you@example.com>
2023-12-12 17:50:29 +0300

    hero

--- interactivepage/components/hero.jsx
+++ interactivepage/components/hero.jsx
@@ -13,7 +13,21 @@ const Hero = () => {
           className="w-1/3 h-screen"
         />
         <div className="absolute flex flex-col  gap-y-6 -mt-[35rem] ms-[15rem]">
-          <div className="bg-[url('/images/bg-card-front.png')] bg-cover w-[400px] h-[220px] rounded-md"></div>
+          <div className="bg-[url('/images/bg-card-front.png')] bg-cover w-[400px] h-[220px] rounded-md">
+            <Image
+              src="/images/card-logo.svg"
+              width={65}
+              height={65}
+              className="ms-[20px] mt-[30px]"
+            />
+            <div className="text-3xl text-white mt-[65px] ms-[20px]">
+              9765 3214 2536 9871
+            </div>
+            <div className="flex text-white justify-between text-sm mt-[15px]">
+              <h1 className="ms-[20px]">Salih ASLAN</h1>
+              <span className="me-[20px]">09/10</span>
+            </div>
+          </div>
           <div className="bg-[url('/images/bg-card-back.png')] bg-cover w-[400px] h-[220px] rounded-md ms-[5rem]"></div>
         </div>
       </div>
