072b91f0c78a7ca5a443db6ce6fc2e42b5f41b02
Your Name <you@example.com>
2023-12-19 15:00:01 +0300

    tailwind set

--- interactivepage/components/info.jsx
+++ interactivepage/components/info.jsx
@@ -58,6 +58,28 @@ const Info = ({ setValueFromFirst, setValueFromSecond }) => {
     setValueFromFirst(formattedValue);
   };
 
+  const handleExpMonth = (e) => {
+    const monthValue = e.target.value;
+    const isNumeric = /^[1-9]\d{0,1}$/.test(monthValue);
+
+    if (isNumeric || monthValue === "") {
+      const numericInput = parseInt(monthValue, 10);
+
+      if (numericInput > 12) {
+        setErrorMessage((prevError) => ({
+          ...prevError,
+          expMonth: "month",
+        }));
+      } else {
+        setErrorMessage((prevError) => ({
+          ...prevError,
+          expMonth: "",
+        }));
+        setExpMonth(monthValue);
+      }
+    }
+  };
+
   return (
     <div>
       <div className="ms-[3rem] mt-[10rem]">
@@ -97,40 +119,54 @@ const Info = ({ setValueFromFirst, setValueFromSecond }) => {
               )}
             </div>
           </div>
-          <div className="  text-sm mt-5 flex ">
-            <div>
-              <label htmlFor="date">EXP. DATE (MM-YY)</label>
-              <div>
-                <input
-                  type="text"
-                  id="date"
-                  name="expMonth"
-                  className={`w-2/12 h-[35px] p-2 mt-1 outline-none border rounded-md ${
-                    errorMessage.expMonth ? "border-red-500" : "border-gray-300"
-                  }`}
-                  value={expMonth}
-                />
-
-                <input
-                  type="text"
-                  id="ydate"
-                  name="expYear"
-                  className={`w-2/12 h-[35px] p-2 mt-1 outline-none border rounded-md ms-2 ${
-                    errorMessage.expYear ? "border-red-500" : "border-gray-300"
-                  }`}
-                  value={expYear}
-                />
+          <div className=" w-1/2  text-sm mt-5 grid grid-cols-2 ">
+            <div className="w-1/1 flex flex-col">
+              <label htmlFor="date">EXP.DATE (MM-YY)</label>
+              <div className="flex">
+                <div>
+                  <input
+                    type="text"
+                    id="date"
+                    name="expMonth"
+                    className={`w-4/5 h-[35px] p-2 mt-1 outline-none border rounded-md ${
+                      errorMessage.expMonth
+                        ? "border-red-500"
+                        : "border-gray-300"
+                    }`}
+                    value={expMonth}
+                    onChange={handleExpMonth}
+                  />
+                  {errorMessage.expMonth && (
+                    <p style={{ color: "red" }}>{errorMessage.expMonth}</p>
+                  )}
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    id="ydate"
+                    name="expYear"
+                    className={`w-4/5 h-[35px] p-2 mt-1 outline-none border rounded-md -ms-2  ${
+                      errorMessage.expYear
+                        ? "border-red-500"
+                        : "border-gray-300"
+                    }`}
+                    value={expYear}
+                  />
+                  {errorMessage.expYear && (
+                    <p style={{ color: "red" }}>{errorMessage.expYear}</p>
+                  )}
+                </div>
               </div>
             </div>
-            <div className=" flex flex-col -ms-[230px]   ">
-              <label htmlFor="cvc" className="ms-2">
+            <div className="w-1/3">
+              <label htmlFor="cvc" className="">
                 CVC
               </label>
               <input
                 type="text"
                 id="cvc"
                 name="cvc"
-                className="w-1/1 h-[35px] p-2 mt-1 outline-none border rounded-md ms-2"
+                className="w-1/1 h-[35px] p-2 mt-1 outline-none border rounded-md -ms-2 "
                 value={cvc}
               />
             </div>
